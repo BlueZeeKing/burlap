dab629b0fca4e1e6f970803783949ae4e94d2676
BlueZeeKing <github@blueish.dev>
2022-08-31 22:32:43 -0400

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../global.css"
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl">Welcome to the Burlap beta!</h1>
+      <Button>Hello!</Button>
+    </div>
+  );
+}
