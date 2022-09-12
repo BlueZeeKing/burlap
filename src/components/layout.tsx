23cab991f54d4ef300acfcc855c9208f66d20945
BlueZeeKing <github@blueish.dev>
2022-09-11 22:09:47 -0400

    Update logo and add it to header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import { getData } from "../lib/fetch";
+import { Course } from "../types/api";
 import Header from "./header";
 import Loader from "./loader";
 import Sidebar from "./sider";
 
 export function CourseLayout(props: {isSuccess: boolean; children: ReactNode}) {
+  const router = useRouter()
+  const { data } = useQuery(["courses", router.query.course], async () => await getData<Course>("courses/"+router.query.course));
+  
   return (
     <div className="h-screen flex flex-col">
-      <Header />
+      <Header text={data?.name} />
 
       <Sidebar>{props.isSuccess ? props.children : <Loader />}</Sidebar>
     </div>
